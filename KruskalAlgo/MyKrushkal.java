c8e8b583c601edd0192d72c8802c4c96304bf316
Ankit <savaliyaankit5125@gmail.com>
2025-07-23 22:34:31 +0530

    created my krushkal algo

--- KruskalAlgo/MyKrushkal.java
+++ KruskalAlgo/MyKrushkal.java
@@ -2,23 +2,25 @@ package KruskalAlgo;
 
 import java.util.Arrays;
 
-public class Kruskal {
+public class MyKrushkal {
+
     public static void main(String[] args) {
-        int V = 4; // number of vertices
+        int V = 4;
         int[][] edges = {
             {0, 1, 10}, {0, 2, 6}, {0, 3, 5}, {1, 3, 15}, {2, 3, 4}
         };
 
         int[] parent = new int[V];
-        for (int i = 0; i < V; i++)
+        for (int i = 0; i < parent.length; i++)
             parent[i] = i;
 
-        // Sort edges by weight
-        Arrays.sort(edges, (a, b) -> a[2] - b[2]);
+        Arrays.sort(edges, (a,b) -> a[2] - b[2]);
+        //  {2, 3, 4}, {0, 3, 5},  {0, 2, 6}, {0, 1, 10}, {1, 3, 15},
 
-        System.out.println("Minimum Spanning Tree (MST):");
+        System.out.println("Minimum Spanning Tree : ");
         int count = 0;
-        for (int i = 0; i < edges.length && count < V - 1; i++) {
+        int tcost = 0;
+        for (int i = 0; i < edges.length && count < V -1 ; i++) {
             int src = edges[i][0];
             int dest = edges[i][1];
             int weight = edges[i][2];
@@ -27,20 +29,25 @@ public class Kruskal {
             int set2 = find(parent, dest);
 
             if (set1 != set2) {
-                System.out.println(src + " -- " + dest + " == " + weight);
+                System.out.println(src + "-" + dest + " : " + weight);
                 union(parent, set1, set2);
+                tcost += weight;
                 count++;
             }
         }
+
+        System.out.println("Total Weight : " + tcost);
+
     }
 
-    static int find(int[] parent, int i) {
-        while (parent[i] != i)
+    static int find(int[] parent, int i){
+        while (parent[i] != i) {
             i = parent[i];
+        }
         return i;
     }
 
-    static void union(int[] parent, int x, int y) {
+    static void union(int[] parent, int x, int y){
         parent[x] = y;
     }
 }
