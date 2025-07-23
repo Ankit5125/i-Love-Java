6e4d6218096788551593c86c9fe3811aea8a3655
Ankit <savaliyaankit5125@gmail.com>
2025-07-24 00:58:30 +0530

    Graph Using List

---
+++ LearningGraph/Graph.java
@@ -0,0 +1,86 @@
+package LearningGraph;
+
+public class Graph {
+
+    static int[][] adjMatrix;
+
+    Graph(int nodes) {
+        adjMatrix = new int[nodes][nodes];
+    }
+
+    public void addEdgesInMatrix(int[][] edges, boolean isDirected){
+        // edges = [[0,2], [1,2], [1,0]]
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+
+            if (isDirected) {
+                adjMatrix[u][v] = 1;
+            }
+            else{
+                adjMatrix[u][v] = 1;
+                adjMatrix[v][u] = 1;
+            }
+        }
+    }
+
+    public void printMatrix(){
+        for (int[] row : adjMatrix) {
+            for (int ele : row) {
+                System.out.print(ele+ " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void addEdgeWithWeight(int[][] edges, boolean isDirected){
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int weight = edge[2];
+
+            if (isDirected) {
+                adjMatrix[u][v] = edge[2];
+            }
+            else{
+                adjMatrix[u][v] = edge[2];
+                adjMatrix[v][u] = edge[2];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // int[][] edges = {
+        //     {0,2},{1,2},{1,0}
+        // };
+
+        int nodes = 3;
+
+        // System.out.println("Undirected : ");
+        // // undirected
+        // Graph graph = new Graph(nodes);
+        // graph.addEdgesInMatrix(edges, false);
+        // graph.printMatrix();
+
+        // System.out.println("Directed : ");
+        // // directed
+        // Graph graph2 = new Graph(nodes);
+        // graph2.addEdgesInMatrix(edges, true);
+        // graph2.printMatrix();
+
+         int[][] edges = {
+            {0,2,10},{1,2,7},{1,0,15}
+        };
+
+        System.out.println("Directed Graph with Weight: ");
+        Graph graph = new Graph(nodes);
+        graph.addEdgeWithWeight(edges, false);
+        graph.printMatrix();
+
+        System.out.println("Undirected Graph with Weight: ");
+        Graph graph2 = new Graph(nodes);
+        graph2.addEdgeWithWeight(edges, true);
+        graph2.printMatrix();
+    }
+}
---
+++ LearningGraph/LeaningGraph.java
@@ -0,0 +1,59 @@
+package LearningGraph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeaningGraph {
+    public static void main(String[] args) {
+
+        int n = 3;
+
+        int[][] edges = {
+                { 0, 2, 10 }, { 1, 2, 7 }, { 1, 0, 15 }
+        };
+        
+        // undirected
+        System.out.println("Undirected Graph : ");
+        MyGraph graph = new MyGraph(n);
+        graph.addEdges(edges, false);
+        graph.printList();
+        
+        // directed
+        System.out.println("Directed Graph : ");
+        MyGraph graph2 = new MyGraph(n);
+        graph2.addEdges(edges, true);
+        graph2.printList();
+    }
+}
+ 
+class MyGraph{
+    List<List<Integer>> adjList;
+ 
+    MyGraph(int n){
+        adjList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+    }
+
+    public void addEdges(int[][] edges, boolean isDirected){
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            if (isDirected) {
+                adjList.get(u).add(v);
+            }
+            else{
+                adjList.get(u).add(v);
+                adjList.get(v).add(u);
+            }
+        }
+    }
+
+    public void printList(){
+        for (int i = 0; i < adjList.size(); i++) {
+            System.out.println(i + " -> " + adjList.get(i));
+        }
+    }
+}
