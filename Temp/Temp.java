98895c8a3010997da80bfb8c8e6728f7ef6f955f
Ankit <savaliyaankit5125@gmail.com>
2025-07-31 19:54:07 +0530

    recursion

--- Temp/Temp.java
+++ Temp/Temp.java
@@ -1,51 +1,10 @@
 public class Temp {
     public static void main(String[] args) {
         
-        // long start = System.nanoTime();
-        // System.out.println(febo(50));
-        // long end = System.nanoTime();
+        long start = System.nanoTime();
+        System.out.println(febo(50));
+        long end = System.nanoTime();
 
-        // System.out.println("Recursion : " + (end - start));
-
-        // long start2 = System.nanoTime();
-        // System.out.println(feboIteration(50));
-        // long end2 = System.nanoTime();
-
-        // System.out.println("Iteration : " + (end2-start2));
-
-        // -----------------------------------------------------------------------------------
-        // int two = 2;
-        // System.out.println(two ^ 1);
-        // System.out.println(~two);
-
-        // -----------------------------------------------------------------------------------
-        int a = 10;
-        int b = 3;
-        long start, end;
-
-        start = System.nanoTime();
-        System.out.println(a * Math.pow(2, b));
-        end = System.nanoTime();
-        System.out.println("Time Taken : " + (end - start));
-        
-        start = System.nanoTime();
-        System.out.println(iterativePower(a,b));
-        end = System.nanoTime();
-        System.out.println("Time Taken : " + (end - start));
-        
-        start = System.nanoTime();
-        System.out.println(a << b);
-        end = System.nanoTime();
-        System.out.println("Time Taken : " + (end - start));
-
-        System.out.println(a);
-    }
-
-    static int iterativePower(int a, int b) {
-        for (int i = 0; i < b; i++) {
-            a *= 2;
-        }
-        return a;
     }
 
     static long feboIteration(int n){
