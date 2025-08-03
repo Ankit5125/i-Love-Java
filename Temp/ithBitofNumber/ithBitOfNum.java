4cd9e6d37f4c6de695cf552fcfa3890fca568f00
Ankit <savaliyaankit5125@gmail.com>
2025-08-03 19:42:23 +0530

    added i'th bit

---
+++ Temp/ithBitofNumber/ithBitOfNum.java
@@ -0,0 +1,12 @@
+package ithBitofNumber;
+
+public class ithBitOfNum {
+    public static void main(String[] args) {
+        
+        int num = 17;
+
+        int eklakh = 1 << 4;
+        System.out.println(num|1);
+        System.out.println(eklakh);
+    }
+}
