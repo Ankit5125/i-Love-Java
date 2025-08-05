069ba972baabadf932bea0c1779d49e1ee74c6d4
Ankit <savaliyaankit5125@gmail.com>
2025-08-05 22:47:31 +0530

    added all the index of target in array using recursion

--- Recursion/Medium/Array_LinearSearch.java
+++ Recursion/Medium/Array_LinearSearch.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Array_LinearSearch {
     public static void main(String[] args) {
 
@@ -6,7 +8,20 @@ public class Array_LinearSearch {
 
         System.out.println(linearSearch(arr, target, 0));
         System.out.println(linearSearchFromBack(arr, target, arr.length-1));
+        System.out.println(linearSearchAllIndex(arr, target, 0));
 
+    }
+
+    static ArrayList<Integer> ans = new ArrayList<>();
+    public static ArrayList<Integer> linearSearchAllIndex(int[] arr, int target, int start) {
+        if (start == arr.length - 1) {
+            return ans;
+        }
+
+        if (arr[start] == target) {
+            ans.add(start);
+        }
+        return linearSearchAllIndex(arr, target, ++start);
     }
 
     public static int linearSearchFromBack(int[] arr, int target, int last) {
