a40ff52a25c893b5877694805afe43c6a58c0aeb
Ankit <savaliyaankit5125@gmail.com>
2025-08-05 22:42:36 +0530

    Linear Search from Back Recusion

--- Recursion/Medium/Array_LinearSearch.java
+++ Recursion/Medium/Array_LinearSearch.java
@@ -1,11 +1,24 @@
 public class Array_LinearSearch {
     public static void main(String[] args) {
 
-        int[] arr = { 1, 5, 50, 8, 15, 16, 85, 90, 50, 75 };
-        int target = 14;
+        int[] arr = { 1, 5, 50, 8, 15, 16, 85, 90, 50, 15, 75 };
+        int target = 15;
 
         System.out.println(linearSearch(arr, target, 0));
+        System.out.println(linearSearchFromBack(arr, target, arr.length-1));
 
+    }
+
+    public static int linearSearchFromBack(int[] arr, int target, int last) {
+        if (last == -1) {
+            return last;
+        }
+
+        if (arr[last] == target) {
+            return last;
+        }
+        else
+            return linearSearch(arr, target, --last);
     }
 
     static int linearSearch(int[] arr, int target, int start){
