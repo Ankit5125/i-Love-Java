3ec2548b6ec3afbd252d5b6628cb811d9537a6aa
Ankit <savaliyaankit5125@gmail.com>
2025-08-08 22:35:02 +0530

    added another method

--- Backtracking/PathToReachDestination.java
+++ Backtracking/PathToReachDestination.java
@@ -3,6 +3,20 @@ package Backtracking;
 public class PathToReachDestination {
     public static void main(String[] args) {
         System.out.println(printPath(3,3, ""));
+        // methodTwo(3, 3, "");
+    }
+
+    static void methodTwo(int cRow, int cCol, String ans){
+        if (cRow == 1 && cCol == 1) {
+            System.out.println(ans);
+        }
+
+        if (cRow>1) {
+            methodTwo(cRow-1, cCol, ans + 'D');
+        }
+        if (cCol > 1) {
+            methodTwo(cRow, cCol-1, ans + 'R');
+        }
     }
 
     static String printPath(int cRow, int cCol, String cAns){
