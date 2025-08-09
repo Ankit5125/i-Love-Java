6ddcdc18657697c1860e4bf1391c582546af528c
Ankit <savaliyaankit5125@gmail.com>
2025-08-10 00:26:09 +0530

    modified all paths maze

--- Backtracking/AllPathsOpen/AllPaths.java
+++ Backtracking/AllPathsOpen/AllPaths.java
@@ -2,28 +2,46 @@ package Backtracking.AllPathsOpen;
 
 public class AllPaths {
     public static void main(String[] args) {
-        printAllPaths(1,1,"");
+
+        int n = 3;
+        Boolean[][] maze = new Boolean[n][3];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                maze[i][j] = true;
+            }
+        }
+
+        printAllPaths(0,0, maze, "");
     }
 
-    static void printAllPaths(int row, int col, String ans){
-        if (row == 3 && col == 3) {
+    static void printAllPaths(int row, int col,Boolean[][] maze, String ans){
+        int n = maze.length-1;
+        if (row == n && col == n) {
             System.out.println(ans);
             return;
         }
 
-        if (row < 2) {
-            printAllPaths(row+1, col, ans + "D");
+        if (!maze[row][col]) {
+            return;
         }
 
-        if (col < 2) {
-            printAllPaths(row, col+1, ans + "R");
+        maze[row][col] = false;
+
+        if (row < n) {
+            printAllPaths(row+1, col,maze , ans + "D");
+        }
+
+        if (col < n) {
+            printAllPaths(row, col+1, maze,ans + "R");
         }
 
         if (row > 0) {
-            printAllPaths(row-1, col, ans+"U");
+            printAllPaths(row-1, col,maze, ans+"U");
         }
         if (col> 0) {
-            printAllPaths(row, col-1, ans+"L");
+            printAllPaths(row, col-1, maze, ans+"L");
         }
+
+        maze[row][col] = true;
     }
 }
