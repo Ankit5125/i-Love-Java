4716f516ad02f3f5829c2823ae8888c4e02994c0
Ankit <savaliyaankit5125@gmail.com>
2025-08-20 22:56:54 +0530

    added doubly linkedlist file

---
+++ LinkedList/DoublyLinkedList.java
@@ -0,0 +1,88 @@
+package LinkedList;
+
+public class DoublyLinkedList {
+    static class Node {
+        int value;
+        Node next;
+        Node previous;
+
+        Node(int value) {
+            this.value = value;
+        }
+
+        Node(int value, Node next, Node previous) {
+            this.value = value;
+            this.next = next;
+            this.previous = previous;
+        }
+    }
+
+    Node head;
+    Node tail;
+    public int size;
+
+    public void addFirst(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.previous = newNode;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void addLast(int value) {
+        Node newNode = new Node(value);
+        newNode.previous = tail;
+        tail.next = newNode;
+        tail = newNode;
+        size++;
+    }
+
+    public void insert(int index, int value) {
+        if (index > size) {
+            System.out.println("Invalid Index");
+            return;
+        } else if (index == 0) {
+            addFirst(value);
+            return;
+        } else if (index == size) {
+            addLast(value);
+        } else {
+            Node newNode = new Node(value);
+            Node temp = head;
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp.next;
+            }
+            newNode.next = temp.next;
+            newNode.previous = temp;
+            if (temp.next != null) {
+                temp.next.previous = newNode;
+            }
+            temp.next = newNode;
+            size++;
+        }
+    }
+
+    public void reverse() {
+        Node temp = tail;
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.previous;
+        }
+        System.out.println("END");
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node temp = head;
+        while (temp != null) {
+            sb.append(temp.value + " -> ");
+            temp = temp.next;
+        }
+        return sb.toString() + "END";
+    }
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+import LinkedList.DoublyLinkedList;
 import LinkedList.LinkedList;
 
 public class Main {
@@ -8,25 +9,39 @@ public class Main {
 
         // CustomeArrayList myList = new CustomeArrayList();
         // for (int i = 0; i < 10; i++) {
-        //     int ele = sc.nextInt();
-        //     myList.add(ele);
+        // int ele = sc.nextInt();
+        // myList.add(ele);
         // }
 
         // System.out.println(myList.toString());
         // System.out.println(myList.totalLength);
         // System.out.println(myList.length);
 
-        LinkedList list = new LinkedList();
+        // ---------------------------------------------------------------------------------
+
+        // LinkedList list = new LinkedList();
+        // list.addFirst(10);
+        // list.addFirst(20);
+        // list.addFirst(30);
+        // list.addLast(40);
+        // list.insert(2, 50);
+
+        // // list.deleteFirst();
+        // list.deleteLast();
+
+        // ---------------------------------------------------------------------------------
+
+        DoublyLinkedList list = new DoublyLinkedList();
         list.addFirst(10);
         list.addFirst(20);
         list.addFirst(30);
         list.addLast(40);
-        list.insert(2, 50);
-
-        // list.deleteFirst();
-        list.deleteLast();
+        list.addLast(50);
+        list.insert(3, 100);
 
         System.out.println("List : " + list.toString());
+        System.out.print("Reverse : ");
+        list.reverse();
         System.out.println("Size : " + list.size);
 
         sc.close();
