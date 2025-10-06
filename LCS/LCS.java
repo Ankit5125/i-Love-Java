35c75550f9ce33372a7360b3c3d835a869968cbe
Ankit <savaliyaankit5125@gmail.com>
2025-10-06 11:36:00 +0530

    lcs final

--- LCS/LCS.java
+++ LCS/LCS.java
@@ -1,7 +1,7 @@
 package LCS;
 
 public class LCS {
-    public static int lcs(String X, String Y) {
+    public static String lcs(String X, String Y) {
         int m = X.length();
         int n = Y.length();
         String[][] dp = new String[m + 1][n + 1];
@@ -12,7 +12,7 @@ public class LCS {
                 if (X.charAt(i - 1) == Y.charAt(j - 1)) {
                     dp[i][j] = (dp[i - 1][j - 1] + 1) + 'C';
                 } else {
-                    dp[i][j] = (Math.max(dp[i - 1][j], dp[i][j - 1])) + dp[i-1][j] >= dp[i][j-1] ? 'U' : 'L';
+                    dp[i][j] = Math.max( dp[i - 1][j], dp[i][j - 1]) + dp[i-1][j] >= dp[i][j-1] ? "U" : "L";
                 }
             }
         }
@@ -45,7 +45,27 @@ public class LCS {
     public static void main(String[] args) {
         String X = "ABCBDAB";
         String Y = "BDCAB";
-        int length = lcs(X, Y);
+        String length = lcs(X, Y);
         System.out.println("\nLength of LCS is: " + length);
+        System.out.println("Longest Common Subsequence is: " + reconstructLCS(length, X, Y));
+    }
+
+    static String reconstructLCS(String[][] dp, String X, String Y) {
+        StringBuilder lcs = new StringBuilder();
+        int i = X.length(), j = Y.length();
+
+        while (i > 0 && j > 0) {
+            if (X.charAt(i - 1) == Y.charAt(j - 1)) {
+                lcs.append(X.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j].length() >= dp[i][j - 1].length()) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+
+        return lcs.reverse().toString();
     }
 }
