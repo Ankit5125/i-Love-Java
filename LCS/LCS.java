4ddef629cc5af4502db1b8ec56394a871d6ddcc5
Ankit Savaliya <167634896+Ankit5125@users.noreply.github.com>
2025-10-06 10:28:29 +0530

    Update LCS.java

--- LCS/LCS.java
+++ LCS/LCS.java
@@ -4,15 +4,15 @@ public class LCS {
     public static int lcs(String X, String Y) {
         int m = X.length();
         int n = Y.length();
-        int[][] dp = new int[m + 1][n + 1];
+        String[][] dp = new String[m + 1][n + 1];
 
         // Fill DP table
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (X.charAt(i - 1) == Y.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    dp[i][j] = (dp[i - 1][j - 1] + 1) + 'C';
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = (Math.max(dp[i - 1][j], dp[i][j - 1])) + dp[i-1][j] >= dp[i][j-1] ? 'U' : 'L';
                 }
             }
         }
